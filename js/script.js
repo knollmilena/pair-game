2305b03c1be0bce348d8c7079a3529cef951c145
knollmilena <50546237+knollmilena@users.noreply.github.com>
2023-05-23 19:43:06 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@
   const allCards = document.querySelectorAll(".cards");
   const container = document.querySelector(".container");
 
-  // Создаем массив с числами
   function fillArray(n) {
     let array = [];
     for (let i = 1; i <= n; i += 1) array.push(i, i);
@@ -12,7 +11,6 @@
     return array;
   }
 
-  // Перемещиваем массив с числами
   function shuffle(arr) {
     let j;
     let temp;
@@ -26,7 +24,6 @@
   }
   const randomArr = shuffle(fillArray(8));
 
-  // Создание карточек
   function createCards() {
     for (let i = 0; i < 16; i++) {
       const div = document.createElement("div");
@@ -36,60 +33,46 @@
     }
   }
   createCards();
-  // Прослушка на клик
-  container.addEventListener("click", (ev) => {
-    const activCard = document.querySelectorAll(".cards");
 
-    if (
-      ev.target !== container &&
-      !ev.target.classList.contains("empty") &&
-      !secondOpenCard()
-    ) {
+  container.addEventListener("click", (ev) => {
+    console.log(ev.target.classList.contains("cards"));
+    if (ev.target.classList.contains("cards") && !bothCardsOpen()) {
       ev.target.classList.add("card--active");
-      if (secondOpenCard()) {
-        doTheNumbersMatch();
+      if (bothCardsOpen()) {
+        matchNumbers();
       }
     }
-    if (container.lastElementChild.classList.contains("btn-play")) {
-      // ЗДЕСЬ ПЕРЕЗАПУСК ИГРЫ
+    if (ev.target.classList.contains("btn-play")) {
       restarGame();
     }
   });
 
-  // Открыто две карточки? да или нет
-  function secondOpenCard() {
+  const bothCardsOpen = () =>
+    document.querySelectorAll(".card--active").length === 2;
+
+  function closeCards() {
     const activCard = document.querySelectorAll(".card--active");
-    if (activCard.length === 2) {
-      return true;
-    } else {
-      return false;
+    activCard[0].classList.remove("card--error", "card--active");
+    activCard[1].classList.remove("card--error", "card--active");
+  }
+
+  function removeRightCards() {
+    const activCard = document.querySelectorAll(".card--active");
+    activCard[0].classList.add("empty");
+    activCard[1].classList.add("empty");
+    activCard[0].classList.remove("cards", "card--active", "card--right");
+    activCard[1].classList.remove("cards", "card--active", "card--right");
+
+    const emptyCards = document.querySelectorAll(".empty");
+
+    if (emptyCards.length === 16) {
+      const buttonRevers = document.createElement("button");
+      buttonRevers.classList.add("btn-play");
+      buttonRevers.appendChild(document.createTextNode("Game again"));
+      container.append(buttonRevers);
     }
   }
-  secondOpenCard();
-
-  //Совпадают ли цифры на карточках?
-  function doTheNumbersMatch() {
-    // Функция закрытия карточек, у которых НЕ совпали цифры
-    function closeTheCard() {
-      activCard[0].classList.remove("card--error", "card--active");
-      activCard[1].classList.remove("card--error", "card--active");
-    }
-    // Функция удаления карточек с поля, если у них совпали цифры
-    function removeRightCards() {
-      activCard[0].classList.add("empty");
-      activCard[1].classList.add("empty");
-      activCard[0].classList.remove("cards", "card--active", "card--right");
-      activCard[1].classList.remove("cards", "card--active", "card--right");
-
-      const emptyCards = document.querySelectorAll(".empty");
-
-      if (emptyCards.length === 16) {
-        const buttonRevers = document.createElement("button");
-        buttonRevers.classList.add("btn-play");
-        buttonRevers.appendChild(document.createTextNode("Game again"));
-        container.append(buttonRevers);
-      }
-    }
+  function matchNumbers() {
     const activCard = document.querySelectorAll(".card--active");
     if (activCard[0].textContent === activCard[1].textContent) {
       activCard[0].classList.add("card--right");
@@ -100,11 +83,10 @@
       activCard[0].classList.add("card--error");
       activCard[1].classList.add("card--error");
 
-      setTimeout(closeTheCard, 500);
+      setTimeout(closeCards, 500);
     }
   }
 
-  // Рестарт игры
   function restarGame() {
     const cardsEmpty = document.querySelectorAll(".empty");
     const buttonRevers = document.querySelector("btn-play");
